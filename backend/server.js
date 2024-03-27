1c3322e948c1cb313150be032a8e24d8f620355b
Thomas joseph <tj089502@gmail.com>
2024-03-27 16:34:25 +0530

    changrs made

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,13 @@ import cookieParser from "cookie-parser";
 import adminRoutes from "./routes/adminRoutes.js";
 import trainerRoutes from "./routes/trainerRoutes.js"; // Add the missing semicolon here
 import cors from "cors";
-
+import path from "path";
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
 // app.use(cors({ origin: "https://stayfit-front-end.vercel.app", credentials: true }));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,13 +23,20 @@ app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/trainer", trainerRoutes); // Add the forward slash before api/trainer
 
-app.get("/", (req, res) => res.send("server is ready"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'success', message: 'Health check passed' });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("server is ready"));
+}
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "success", message: "Health check passed" });
 });
-
-
 
 app.use(notFound);
 app.use(errorHandler);
@@ -46,7 +52,6 @@ const io = new Server(server, {
   cors: {
     // origin: ["https://stayfit-front-end.vercel.app"],
     origin: ["http://localhost:3000"],
-
   },
 });
 
@@ -83,4 +88,4 @@ io.on("connection", (socket) => {
   });
 });
 
-export  {app,io};
+export { app, io };
